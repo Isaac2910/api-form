ce4d6039f430ead580d78f4516046aa8e0750eb9
Newton10 <isaacomar29@gmail.com>
2024-09-21 20:28:43 +0100

    Update app.js
    cors

--- app.js
+++ app.js
@@ -5,6 +5,13 @@ import routeReservation from './routes/reservationsRoutes.js'
 const port = process.env.PORT || 3500
 dotenv.config();
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
+
 const app = express();
 
 app.use(express.json())
